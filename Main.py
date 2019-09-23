553f283f47cd5ac92e4bc21e767b88a29dba266c
NickFreberg <nfreberg@student.bridgew.edu>
2019-09-23 16:42:05 -0400

    Managed to get multiple images on the screen. Next, I need to work on scrolling and shooting

--- Main.py
+++ Main.py
@@ -2,6 +2,12 @@ import arcade
 import pathlib
 from enum import auto, Enum
 
+# CONSTANTS
+
+PLAYER_SPEED = 5
+GRAVITY = 1
+JUMP_SPEED = 15
+
 
 class MoveEnum(Enum):
     NONE = auto()
@@ -12,32 +18,73 @@ class MoveEnum(Enum):
 
 
 class MinimalSprite(arcade.Sprite):
-    def __init__(self, ship_path: str, speed:int, game_window):
+    def __init__(self, ship_path: str, speed: int, game_window):
         super().__init__(ship_path)
         self.speed = speed
         self.game = game_window
 
-    def move(self, direction: MoveEnum):
+    def move(self, list:arcade.SpriteList,direction: MoveEnum):
         # as a class exercise, lets fix this so it doesn't go off the window
         if direction == MoveEnum.UP:
-            self.center_y += self.speed
+            for thing in list:
+                if self.top >= thing.bottom:
+                    pass
+                else:
+                    self.center_y += PLAYER_SPEED
             print(self.center_x, self.center_y)
         elif direction == MoveEnum.DOWN:
-            self.center_y -= self.speed
+            self.center_y -= PLAYER_SPEED
             print(self.center_x, self.center_y)
         elif direction == MoveEnum.LEFT:
-            self.center_x -=self.speed
+            self.center_x -= PLAYER_SPEED
             print(self.center_x, self.center_y)
         elif direction == MoveEnum.RIGHT:
-            self.center_x += self.speed
+            self.center_x += PLAYER_SPEED
             print(self.center_x, self.center_y)
         else:  # should be MoveEnum.NONE
             pass
 
 
+def move(sprite: arcade.Sprite, slist: arcade.SpriteList, direction: MoveEnum):
+    # as a class exercise, lets fix this so it doesn't go off the window
+    if direction == MoveEnum.UP:
+        sprite.center_y += PLAYER_SPEED
+        print("Top: " + str(sprite.top) +
+              "\nBottom: " + str(sprite.bottom) +
+              "\nLeft: " + str(sprite.left) +
+              "\nRight: " + str(sprite.right))
+
+    elif direction == MoveEnum.DOWN:
+        for thing in slist:
+            if float(sprite.bottom) > float(thing.top):
+                if not thing.collides_with_sprite(sprite):
+                    sprite.center_y -= PLAYER_SPEED
+        print("Top: " + str(sprite.top) +
+              "\nBottom: " + str(sprite.bottom) +
+              "\nLeft: " + str(sprite.left) +
+              "\nRight: " + str(sprite.right))
+    elif direction == MoveEnum.LEFT:
+        sprite.center_x -= PLAYER_SPEED
+        print("Top: " + str(sprite.top) +
+
+              "\nBottom: " + str(sprite.bottom) +
+
+              "\nLeft: " + str(sprite.left) +
+
+              "\nRight: " + str(sprite.right))
+    elif direction == MoveEnum.RIGHT:
+        sprite.center_x += PLAYER_SPEED
+        print("Top: " + str(sprite.top) +
+              "\nBottom: " + str(sprite.bottom) +
+              "\nLeft: " + str(sprite.left) +
+              "\nRight: " + str(sprite.right))
+    else:  # should be MoveEnum.NONE
+        pass
+
+
 class MinimalArcade(arcade.Window):
 
-    def __init__(self, image_name:str, screen_w:int = 1024, screen_h:int =1024):
+    def __init__(self, image_name: str, screen_w: int = 1024, screen_h: int = 750):
         super().__init__(screen_w, screen_h)
         self.image_path = pathlib.Path.cwd() / 'Assets' / image_name
         self.pict = None
@@ -45,7 +92,7 @@ class MinimalArcade(arcade.Window):
         self.pictlist = None
 
     def setup(self):
-        self.pict = MinimalSprite(str(self.image_path), speed = 5, game_window = self)
+        self.pict = MinimalSprite(str(self.image_path), speed=5, game_window=self)
         self.pict.center_x = 500
         self.pict.center_y = 500
         self.pictlist = arcade.SpriteList()
@@ -56,14 +103,11 @@ class MinimalArcade(arcade.Window):
         # adjust the movement, but for this simple version I'll forgo that.
         self.pict.move(self.direction)
 
-
-
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
         # Code to draw the screen goes here
         self.pictlist.draw()
-
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -75,60 +119,93 @@ class MinimalArcade(arcade.Window):
             self.direction = MoveEnum.LEFT
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.direction = MoveEnum.RIGHT
-
+        elif key == arcade.key.SPACE:
+            self.direction
 
     def on_key_release(self, key: int, modifiers: int):
         """called by arcade for keyup events"""
         if (key == arcade.key.UP or key == arcade.key.W) and \
                 self.direction == MoveEnum.UP:
             self.direction = MoveEnum.NONE
-        if (key == arcade.key.DOWN or key == arcade.key.S) and\
+        if (key == arcade.key.DOWN or key == arcade.key.S) and \
                 self.direction == MoveEnum.DOWN:
             self.direction = MoveEnum.NONE
-        if (key == arcade.key.LEFT or key == arcade.key.A) and\
+        if (key == arcade.key.LEFT or key == arcade.key.A) and \
                 self.direction == MoveEnum.LEFT:
             self.direction = MoveEnum.NONE
-        if (key == arcade.key.RIGHT or key == arcade.key.D) and\
+        if (key == arcade.key.RIGHT or key == arcade.key.D) and \
                 self.direction == MoveEnum.RIGHT:
             self.direction = MoveEnum.NONE
 
 
+class ArcadeButWithStuff(arcade.Window):
 
-'''class ArcadeButWithStuff(arcade.Window):
+    def __init__(self, screen_w: int = 1024, screen_h: int = 500):
 
-    def __init__(self, image_names:str[2], screen_w:int = 1024, screen_h:int =1024):
-        # array will contain map image, player image
         super().__init__(screen_w, screen_h)
-        self.map_image_path = pathlib.Path.cwd() / 'Assets' / image_names[0]
+
+        self.bullet_list = None
+        self.wall_list = None
+        self.player_list = None
+
+        self.player_sprite = None
+        self.direction = MoveEnum.NONE
+        self.physics_engine = None
+        self.player_is_shooting = False
+        arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+        '''self.map_image_path = pathlib.Path.cwd() / 'Assets' / image_names[0]
         self.player_image_path = pathlib.Path.cwd() / 'Assets' / image_names[1]
         self.bullet_image_path = pathlib.Path.cwd() / 'Assets' / image_names[2]
         self.map_pict = None
         self.player_pict = None
         self.bullet_pict = None
         self.direction = MoveEnum.NONE
-        self.pictlist = None
+        self.pictlist = None '''
 
     def setup(self):
-        self.pict = MinimalSprite(str(self.image_path), speed=5, game_window=self)
-        self.pict.center_x = 500
-        self.pict.center_y = 500
-        
-        self.map_pict.center_x = 1520
-        self.map_pict.center_y = 720
-        
-        self.pictlist = arcade.SpriteList()
-        self.pictlist.append(self.pict)
+        # Create the Sprite Lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+
+        # set the player at its coordinates
+        self.player_sprite = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / "PlayerSprite1.png")
+        self.player_sprite.center_x = 10
+        self.player_sprite.center_y = 95
+        self.player_list.append(self.player_sprite)
+
+        # Create the ground
+        for i in range(0, 1250, 64):
+            wall = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / 'Tiles' / "GrassUpDirt.png")
+            wall.center_x = i
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        # add a few rocks
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / 'Tiles' / "rock.png")
+            wall.position = coordinate
+            self.wall_list.append(wall)
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
 
     def on_update(self, delta_time: float):
         # to get really smooth movement we would use the delta time to
         # adjust the movement, but for this simple version I'll forgo that.
-        self.pict.move(self.direction)
+        move(self.player_sprite, self.wall_list, self.direction)
+        # self.player_sprite.move(self.direction)
 
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
         # Code to draw the screen goes here
-        self.pictlist.draw()
+        self.wall_list.draw()
+        self.bullet_list.draw()
+        self.player_list.draw()
+
+    def shoot(self):
+        bullet = arcade.Sprite
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -140,24 +217,25 @@ class MinimalArcade(arcade.Window):
             self.direction = MoveEnum.LEFT
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.direction = MoveEnum.RIGHT
+        elif key == arcade.key.SPACE:
+            self.player_is_shooting = True
+            shoot(self.player_sprite)
+            self.player_is_shooting = False
 
     def on_key_release(self, key: int, modifiers: int):
         """called by arcade for keyup events"""
         if (key == arcade.key.UP or key == arcade.key.W) and \
                 self.direction == MoveEnum.UP:
             self.direction = MoveEnum.NONE
-        if (key == arcade.key.DOWN or key == arcade.key.S) and\
+        if (key == arcade.key.DOWN or key == arcade.key.S) and \
                 self.direction == MoveEnum.DOWN:
             self.direction = MoveEnum.NONE
-        if (key == arcade.key.LEFT or key == arcade.key.A) and\
+        if (key == arcade.key.LEFT or key == arcade.key.A) and \
                 self.direction == MoveEnum.LEFT:
             self.direction = MoveEnum.NONE
-        if (key == arcade.key.RIGHT or key == arcade.key.D) and\
+        if (key == arcade.key.RIGHT or key == arcade.key.D) and \
                 self.direction == MoveEnum.RIGHT:
             self.direction = MoveEnum.NONE
-'''
-
-
 
 
 
@@ -167,7 +245,7 @@ def main():
     test_map = MinimalSprite(pathlib.Path.cwd() / 'Assets' / 'TestMap.png')
     bullet = MinimalSprite(pathlib.Path.cwd() / 'Assets' / 'Bullet')    """
 
-    window = MinimalArcade("TestMap.png", screen_w=1080)
+    window = ArcadeButWithStuff(screen_h=920, screen_w=1080)
     # What I wanna do: pass in all the sprites into the ting and go from there
     # window = ArcadeButWithStuff(["TestMap.png", "PlayerSprite1.png", "PlayerBullet.png"], screen_w = 1080)
 
@@ -177,5 +255,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
