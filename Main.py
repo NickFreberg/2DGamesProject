4bccacd96ae16e79a6d2019dc9ca2f26ac3e9cad
NickFreberg <nfreberg@student.bridgew.edu>
2019-10-06 21:43:19 -0400

    Project 1.5: Update:
    + Added Enemies and Sounds
    + Added Loss Condition
    + Added a few more obstacles to serve as spawnpoints for enemies.
    
    Things that need to be cleaned up:
    - Collision with obstacles and enemies
    - Add SpawnEnemy function at a certain frame interval
    - Win Condition

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import arcade
 import pathlib
+import time
 from enum import auto, Enum
 
 # CONSTANTS
@@ -13,6 +14,9 @@ LEFT_VIEWPORT_MARGIN = 150
 RIGHT_VIEWPORT_MARGIN = 150
 BOTTOM_VIEWPORT_MARGIN = 50
 TOP_VIEWPORT_MARGIN = 100
+
+MAX_ENEMIES_ON_SCREEN = 5
+SCORE_TO_GET_ENEMY_2 = 45
 
 
 class MoveEnum(Enum):
@@ -61,10 +65,10 @@ def move(sprite: arcade.Sprite, slist: arcade.SpriteList, direction: MoveEnum):
               "\nRight: " + str(sprite.right))
 
     elif direction == MoveEnum.DOWN:
-        for thing in slist:
-            if float(sprite.bottom) > float(thing.top):
-                if not thing.collides_with_sprite(sprite):
-                    sprite.center_y -= PLAYER_SPEED
+        if sprite.bottom > 63.0:
+            sprite.center_y -= PLAYER_SPEED
+        else:
+            pass
         print("Top: " + str(sprite.top) +
               "\nBottom: " + str(sprite.bottom) +
               "\nLeft: " + str(sprite.left) +
@@ -125,8 +129,6 @@ class MinimalArcade(arcade.Window):
             self.direction = MoveEnum.LEFT
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.direction = MoveEnum.RIGHT
-        elif key == arcade.key.SPACE:
-            self.direction
 
     def on_key_release(self, key: int, modifiers: int):
         """called by arcade for keyup events"""
@@ -152,15 +154,25 @@ class ArcadeButWithStuff(arcade.Window):
 
         self.bullet_list = None
         self.wall_list = None
+        self.obstacle_list = None
         self.player_list = None
+        self.enemy_list = None
+        self.enemy_bullet_list = None
 
         self.player_sprite = None
         self.direction = MoveEnum.NONE
         self.physics_engine = None
         self.player_is_shooting = False
         self.shot_sound = None
+        self.enemy_shot_sound = None
+        self.enemy_hit_sound = None
+        self.player_hit_sound = None
+        self.game_over = None
+        self.win = None
         self.view_bottom = 0
         self.view_left = 0
+
+        self.score = 0
         arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 
         '''self.map_image_path = pathlib.Path.cwd() / 'Assets' / image_names[0]
@@ -174,12 +186,23 @@ class ArcadeButWithStuff(arcade.Window):
 
     def setup(self):
         # Create the Sprite Lists
+        self.frame_count = 0
 
         self.shot_sound = arcade.load_sound(pathlib.Path.cwd() / 'Assets' / "Yeet.wav")
+        self.enemy_shot_sound = arcade.load_sound(pathlib.Path.cwd() / 'Assets' / "EnemyMagic.wav")
+        self.enemy_hit_sound = arcade.load_sound(pathlib.Path.cwd() / 'Assets' / "EnemyHit.wav")
+        self.player_hit_sound = None
+        self.game_over = arcade.load_sound(pathlib.Path.cwd() / 'Assets' / "Game Over.wav")
+        self.win = arcade.load_sound(pathlib.Path.cwd() / 'Assets' / "Win.wav")
+        self.score = 0
+        self.player_dead = False
 
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.obstacle_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
+        self.enemy_bullet_list = arcade.SpriteList()
 
         # set the player at its coordinates
         self.player_sprite = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / "PlayerSprite1.png")
@@ -187,43 +210,106 @@ class ArcadeButWithStuff(arcade.Window):
         self.player_sprite.center_y = 95
         self.player_list.append(self.player_sprite)
 
+        # set initial enemy position
+        self.enemy_sprite = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / "Enemy1.png")
+        self.enemy_sprite.center_x = 269
+        self.enemy_sprite.center_y = 95
+        self.enemy_list.append(self.enemy_sprite)
+
         # Create the ground
-        for i in range(0, 5000, 64):
+        for i in range(-200, 5000, 64):
             wall = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / 'Tiles' / "GrassUpDirt.png")
             wall.center_x = i
             wall.center_y = 32
             self.wall_list.append(wall)
 
         # add a few rocks
-        coordinate_list = [[512, 96], [256, 96], [768, 96], [934, 96], [2000, 96]]
+        coordinate_list = [[512, 96], [768, 96], [934, 96], [2000, 96]]
         for coordinate in coordinate_list:
             wall = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / 'Tiles' / "rock.png")
             wall.position = coordinate
-            self.wall_list.append(wall)
+            self.obstacle_list.append(wall)
+
+        # add the enemy spawn points
+        spawnpoint_list = [[4100, 96], [2905, 96], [1420, 96], [1720, 96], [333, 96]]
+        for coordinate in spawnpoint_list:
+            wall = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / 'Tiles' / "enemyspawnpoint.png")
+            wall.position = coordinate
+            self.obstacle_list.append(wall)
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
 
     def on_update(self, delta_time: float):
+
+        self.frame_count += 1
+        print(self.frame_count)
+
+        # If the current frame divided by 360 has a remainder of 0, have the enemy shoot
+        if self.frame_count % 360 == 0 and self.player_dead == False:
+            self.enemy_shoot()
+        for eb in self.enemy_bullet_list:
+            if self.player_dead == True:
+                arcade.draw_text("Game Over...", 10 + self.view_left, 10 + self.view_bottom, arcade.csscolor.WHITE,
+                                 18)
+                if self.frame_count % 200:
+                    arcade.close_window()
+            for wall in self.wall_list:
+                if arcade.check_for_collision(eb, wall):
+                    eb.remove_from_sprite_lists()
+                    eb.kill
+                if arcade.check_for_collision(eb, self.player_sprite):
+                    eb.remove_from_sprite_lists()
+                    eb.kill
+                    self.player_sprite.remove_from_sprite_lists()
+                    self.player_sprite.kill
+                    self.player_dead = True
+                    arcade.play_sound(self.game_over)
+                    break
+
+                    # self.score += 1
         # to get really smooth movement we would use the delta time to
         # adjust the movement, but for this simple version I'll forgo that.
         move(self.player_sprite, self.wall_list, self.direction)
         self.bullet_list.update()
+        self.enemy_bullet_list.update()
         for bullet in self.bullet_list:
-            if bullet.bottom > self.height or bullet.top < 0:
+            for wall in self.wall_list:
+                if arcade.check_for_collision(bullet, wall):
+                    bullet.remove_from_sprite_lists()
+                    bullet.kill
+                    # wall.remove_from_sprite_lists()
+                    # self.score += 1
+
+            for enemy in self.enemy_list:
+                if arcade.check_for_collision(bullet, enemy):
+                    enemy.remove_from_sprite_lists()
+                    arcade.play_sound(self.enemy_hit_sound)
+                    enemy.kill
+                    enemy.remove_from_sprite_lists
+                    bullet.remove_from_sprite_lists
+                    bullet.kill
+                    self.score += 10
+                if arcade.check_for_collision(enemy, self.player_sprite):
+                    arcade.play_sound(self.game_over)
+                    self.player_sprite.remove_from_sprite_lists()
+                    self.player.kill
+                    frame_of_death = self.frame_count
+                    if self.frame_count >= frame_of_death + 200:
+                        arcade.close_window()
+
+            '''if bullet.bottom > self.height or bullet.top < 0:
                 bullet.kill()
             if bullet.center_x > self.width or bullet.center_x < 0:
-                bullet.kill()
-
-        #Scroll Logic
-
+                bullet.kill()'''
+        # Scroll Logic
         changed = False
-
-        #Scroll to the Left
+        # Scroll to the Left
         left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
         if self.player_sprite.left < left_boundary:
             self.view_left -= left_boundary - self.player_sprite.left
             changed = True
 
-        #Scroll to the Right
+        # Scroll to the Right
         right_boundary = self.view_left + self.width - RIGHT_VIEWPORT_MARGIN
         if self.player_sprite.right > right_boundary:
             self.view_left += self.player_sprite.right - right_boundary
@@ -252,16 +338,46 @@ class ArcadeButWithStuff(arcade.Window):
             arcade.set_viewport(self.view_left, self.width + self.view_left, self.view_bottom, self.height +
                                 self.view_bottom)
 
-
         # self.player_sprite.move(self.direction)
 
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
         # Code to draw the screen goes here
-        self.wall_list.draw()
-        self.bullet_list.draw()
-        self.player_list.draw()
+        if not self.player_dead:
+            self.wall_list.draw()
+            self.obstacle_list.draw()
+            self.bullet_list.draw()
+            self.player_list.draw()
+            self.enemy_list.draw()
+            self.enemy_bullet_list.draw()
+            score_text = f"Score: {self.score}"
+            arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom, arcade.csscolor.WHITE, 18)
+        else:
+            arcade.set_background_color(arcade.csscolor.BLACK)
+            arcade.draw_text("Game Over...", 10 + self.view_left, 10 + self.view_bottom, arcade.csscolor.WHITE, 18)
+
+
+
+    def enemy_shoot(self):
+        for enemy in self.enemy_list:
+            # arcade.sound.play_sound(self.enemy_shot_sound)
+            bullet = arcade.Sprite(pathlib.Path.cwd() / 'Assets' / "enemy magic (bullet).png")
+            # If the enemy is to the left of the player
+            if enemy.center_x < self.player_sprite.center_x:
+                bullet.change_x = BULLET_SPEED
+                bullet.center_x = enemy.center_x + 32
+                bullet.center_y = enemy.center_y + 16
+                self.enemy_bullet_list.append(bullet)
+                arcade.sound.play_sound(self.enemy_shot_sound)
+
+            elif enemy.center_x > self.player_sprite.center_x:
+                bullet.angle = 180
+                bullet.change_x = -BULLET_SPEED
+                bullet.center_x = enemy.center_x - 32
+                bullet.center_y = enemy.center_y + 16
+                self.enemy_bullet_list.append(bullet)
+                arcade.sound.play_sound(self.enemy_shot_sound)
 
     def shoot(self):
 
@@ -296,11 +412,6 @@ class ArcadeButWithStuff(arcade.Window):
             bullet.center_x = self.player_sprite.center_x + 32
             bullet.center_y = self.player_sprite.center_y
 
-
-
-
-
-
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
         if key == arcade.key.UP or key == arcade.key.W:
@@ -330,7 +441,6 @@ class ArcadeButWithStuff(arcade.Window):
             self.direction = MoveEnum.NONE
 
 
-
 def main():
     """ Main method
     player = MinimalSprite(pathlib.Path.cwd() / 'Assets' / 'PlayerSprite1.png')
@@ -338,8 +448,6 @@ def main():
     bullet = MinimalSprite(pathlib.Path.cwd() / 'Assets' / 'Bullet')    """
 
     window = ArcadeButWithStuff(screen_h=920, screen_w=1080)
-    # What I wanna do: pass in all the sprites into the ting and go from there
-    # window = ArcadeButWithStuff(["TestMap.png", "PlayerSprite1.png", "PlayerBullet.png"], screen_w = 1080)
 
     window.setup()
     arcade.run()
